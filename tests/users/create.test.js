e3cb2f6a20bd99b13df1674808f5c3c06a72985d
N Danu <danu.nicu@gmail.com>
2024-03-14 10:55:31 +0100

    initial commit

---
+++ tests/users/create.test.js
@@ -0,0 +1,6 @@
+describe('Given a request to POST /users', ()=> {
+    it('should create a new user with valid data and return a 201 status code', async () => {})
+    it('should return a 422 status code if the user already exists', async () => {})
+    it('should return a 401 status code if access token is missing', async () => {})
+    it('should return a 422 status code if request data is invalid', async () => {})
+})
